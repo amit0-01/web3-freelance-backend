eec929d37b33cd97dfbfa4156337d79c4d1373b5
amit0-01 <your-email@example.com>
2025-04-27 16:24:35 +0530

    moved the pollyfills to up

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import * as nodeCrypto from 'crypto';
 
+// Polyfill must happen BEFORE anything else
 if (!globalThis.crypto) {
   globalThis.crypto = {
     randomUUID: nodeCrypto.randomUUID,
     getRandomValues: (arr: any) => nodeCrypto.randomFillSync(arr),
-    subtle: {} as any, // You can implement subtle if needed
+    subtle: {} as any,
   } as any;
 }
+
+
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +21,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, 
   });
-  await app.listen(8000,'0.0.0.0');
+  await app.listen(process.env.PORT ?? 3000,'0.0.0.0');
 }
 bootstrap();
