b70e0c24dfca3d8f50ac5302c7373bf9de92dd15
amit0-01 <your-email@example.com>
2025-04-27 14:52:28 +0530

    changed the port in main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, 
   });
-  await app.listen(process.env.PORT ?? 3000,'0.0.0.0');
+  await app.listen(8000,'0.0.0.0');
 }
 bootstrap();
