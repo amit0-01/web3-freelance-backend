d2b58e1d0a52104c73360f28ccf6e890cfd74ca1
amit0-01 <your-email@example.com>
2025-04-27 16:21:23 +0530

    added crypto in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as nodeCrypto from 'crypto';
+
+if (!globalThis.crypto) {
+  globalThis.crypto = {
+    randomUUID: nodeCrypto.randomUUID,
+    getRandomValues: (arr: any) => nodeCrypto.randomFillSync(arr),
+    subtle: {} as any, // You can implement subtle if needed
+  } as any;
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
