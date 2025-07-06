87e95603ac9bb4033cf3263c85a048f79c35e7c1
amit0-01 <amit22414@gmail.com>
2025-07-06 22:57:47 +0530

    removed typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,11 @@ import { JobsModule } from './jobs/jobs.module';
 import { BlockchainModule } from './blockchain/blockchain.module'; 
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Job } from './jobs/job.entity';
 import { User } from './user/user.entity';
 import { ConfigModule } from '@nestjs/config';
 import { ChatModule } from './chat/chat.module';
+import { PrismaModule } from './databases/prisma.module';
 
 
 @Module({
@@ -17,32 +17,12 @@ import { ChatModule } from './chat/chat.module';
     ConfigModule.forRoot({
       isGlobal: true, 
     }),
-    TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'postgres',
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT, 10),
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: true,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-        extra: {
-          max: 5, // limit connections (important for serverless)
-          keepAlive: true,
-        },
-        autoLoadEntities: true,
-      }),
-    }),
     JobsModule,
     BlockchainModule, 
     AuthModule, 
     UserModule,
     ChatModule,
-    TypeOrmModule.forFeature([User, Job])
+    PrismaModule
   ],
   controllers: [AppController],
   providers: [AppService],
