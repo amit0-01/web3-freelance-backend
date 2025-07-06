b48db3b97517132b4b563bb7f36f3dd16eab8422
amit0-01 <amit22414@gmail.com>
2025-07-06 11:33:14 +0530

    updated prisma module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
 import { User } from './user.entity';
-import { PrismaModule } from 'src/databases/prisma.module';
+import { PrismaModule } from '../databases/prisma.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), PrismaModule],
