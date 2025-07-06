139b19a631c4d23f0ece47932ff7399490c71a0a
amit0-01 <amit22414@gmail.com>
2025-07-06 22:51:52 +0530

    updated prisa service .ts

--- src/databases/prisma.service.ts
+++ src/databases/prisma.service.ts
@@ -2,11 +2,22 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+let globalPrisma: PrismaClient;
+
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    // ✅ Reuse PrismaClient in serverless environments
+    if (!globalPrisma) {
+      globalPrisma = new PrismaClient();
+    }
+    super();
+    Object.assign(this, globalPrisma);
+  }
+
   async onModuleInit() {
     await this.$connect();
-    console.log('Database connected');
+    console.log('Prisma DB connected (via cached client)');
   }
 
   async onModuleDestroy() {
