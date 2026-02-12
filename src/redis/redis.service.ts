bf3e33783a50b38e5021618dac2c5d764e2063d2
amit0-01 <amit22414@gmail.com>
2026-02-12 22:21:42 +0530

    added production redis cloud

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -5,20 +5,29 @@ import Redis from 'ioredis';
 export class RedisService implements OnModuleInit {
     private client: Redis;
 
-  onModuleInit() {
-    this.client = new Redis({
-      host: process.env.REDIS_HOST || 'localhost',
-      port: Number(process.env.REDIS_PORT) || 6379,
-    });
-
-    this.client.on('connect', () => {
-      console.log('✅ Redis connected');
-    });
-
-    this.client.on('error', (err) => {
-      console.error('❌ Redis error', err);
-    });
-  }
+    onModuleInit() {
+      const redisUrl = process.env.REDIS_URL;
+    
+      if (redisUrl) {
+        // Production (Redis Cloud)
+        this.client = new Redis(redisUrl);
+      } else {
+        // Local Docker Redis
+        this.client = new Redis({
+          host: process.env.REDIS_HOST || 'localhost',
+          port: Number(process.env.REDIS_PORT) || 6379,
+        });
+      }
+    
+      this.client.on('connect', () => {
+        console.log('✅ Redis connected');
+      });
+    
+      this.client.on('error', (err) => {
+        console.error('❌ Redis error', err);
+      });
+    }
+    
 
   async get(key: string) {
     return this.client.get(key);
